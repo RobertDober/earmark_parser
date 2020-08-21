dfac8cb18ab03e469bd73a010d0f9d6b26af4cb1
Ben Olive <sionide21@gmail.com>
2020-08-20 22:11:13 -0400

    Retain whitespace when parsing pure links
    Fixes #22

--- lib/earmark_parser/helpers/pure_link_helpers.ex
+++ lib/earmark_parser/helpers/pure_link_helpers.ex
@@ -7,13 +7,13 @@ defmodule EarmarkParser.Helpers.PureLinkHelpers do
   @pure_link_rgx ~r{\A(\s*)(\()?(https?://[[:alnum:]"'*@:+-_{\}()/.%\#]*)}u
   def convert_pure_link(src) do
     case Regex.run(@pure_link_rgx, src) do
-      [_match, spaces, "", link_text] -> reparse_link(String.length(spaces), link_text)
-      [_match, spaces, _, link_text]  -> remove_trailing_closing_parens(String.length(spaces), link_text) 
+      [_match, spaces, "", link_text] -> reparse_link(spaces, link_text)
+      [_match, spaces, _, link_text]  -> remove_trailing_closing_parens(spaces, link_text)
       _ -> nil
       end
   end
 
-  defp determine_ending_parens_by_count(leading_spaces_count, prefix, surplus_on_closing_parens) do
+  defp determine_ending_parens_by_count(leading_spaces, prefix, surplus_on_closing_parens) do
     graphemes = String.graphemes(prefix)
     open_parens_count = Enum.count(graphemes, &(&1 == "("))
     close_parens_count = Enum.count(graphemes, &(&1 == ")"))
@@ -22,14 +22,20 @@ defmodule EarmarkParser.Helpers.PureLinkHelpers do
     needed =
     :lists.duplicate(max(0, take), ")")
     |> Enum.join
-    {link(prefix <> needed), String.length(prefix) + leading_spaces_count + max(0,take)} 
+    link = link(prefix <> needed)
+    ast =
+      case leading_spaces do
+        "" -> link
+        _ -> [leading_spaces, link]
+      end
+    {ast, String.length(prefix) + String.length(leading_spaces) + max(0,take)}
   end
 
   @split_at_ending_parens ~r{(.*?)(\)*)\z}
-  defp remove_trailing_closing_parens(leading_spaces_count, link_text) do
+  defp remove_trailing_closing_parens(leading_spaces, link_text) do
     [_, _prefix, suffix] = Regex.run(@split_at_ending_parens, link_text)
     case suffix do
-      "" -> {"(", leading_spaces_count + 1}
+      "" -> {"(", String.length(leading_spaces) + 1}
       _  -> case convert_pure_link(betail(link_text, 1)) do
         {link, length} -> {["(", link, ")"], length + 2}
         _ -> nil
@@ -37,10 +43,10 @@ defmodule EarmarkParser.Helpers.PureLinkHelpers do
     end
   end
 
-  defp reparse_link(leading_spaces_count, link_text) do
+  defp reparse_link(leading_spaces, link_text) do
     [_, prefix, suffix] = Regex.run(@split_at_ending_parens, link_text)
     nof_closing_parens = String.length(suffix)
-    determine_ending_parens_by_count(leading_spaces_count, prefix, nof_closing_parens)
+    determine_ending_parens_by_count(leading_spaces, prefix, nof_closing_parens)
   end
 
   defp link(text), do: render_link(text, text)
