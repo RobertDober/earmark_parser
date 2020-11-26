792df74a45d6ba0ce2c3c5b6d18f60eae969dd53
RobertDober <robert.dober@gmail.com>
2020-11-26 15:17:03 +0100

    Release v1.4.11

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule EarmarkParser.MixProject do
   use Mix.Project
 
-  @version "1.5.0"
+  @version "1.4.11"
   @url "https://github.com/RobertDober/earmark_parser"
 
   @deps [
@@ -54,7 +54,7 @@ defmodule EarmarkParser.MixProject do
         "src/*.yrl",
         "mix.exs",
         "README.md",
-        "CHANGELOG.md",
+        "RELEASE.md",
         "LICENSE"
       ],
       maintainers: [
