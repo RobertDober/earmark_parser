ae8c9eb4e955e6fdc624de5b65ce9edba27401d9
Jan Pieper <kontakt@jan-pieper.info>
2024-07-02 09:49:28 +0200

    Add `runtime: false` to `dialyxir` dependency

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule EarmarkParser.MixProject do
 
   @deps [
     {:credo, "~> 1.7.5", only: [:dev]},
-    {:dialyxir, "~> 1.4.1", only: [:dev]},
+    {:dialyxir, "~> 1.4.1", only: [:dev], runtime: false},
     {:earmark_ast_dsl, "~> 0.3.7", only: [:test]},
     {:excoveralls, "~> 0.14.4", only: [:test]},
     {:extractly, "~> 0.5.3", only: [:dev]},
