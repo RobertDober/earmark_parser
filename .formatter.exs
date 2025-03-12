7f1ab5767311d651c20482b3d2dba25c6b1e58bf
RobertDober <robert.dober@gmail.com>
2025-03-12 08:18:34 +0100

    Road to #164
        - added .formatter.exs
        - modified CI to check for formatted, WIP

---
+++ .formatter.exs
@@ -0,0 +1,10 @@
+[
+  force_do_end_blocks: true,
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  line_length: 120,
+  migrate: true,
+  migrate_bitstring_modifiers: true,
+  migrate_charlists_as_sigils: true,
+  migrate_unless: false,
+]
+# SPDX-License-Identifier: Apache-2.0
