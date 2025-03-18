19c70cae88d9a3aecf3cb65b41340f61a711baef
Brad Hanks <hanks.brad@gmail.com>
2025-03-17 23:55:32 -0600

    added EarmarkParser.{Options, Line}; added type specifications for EarmarkParser.Parser.ListInfo

--- lib/earmark_parser/parser/list_info.ex
+++ lib/earmark_parser/parser/list_info.ex
@@ -1,6 +1,6 @@
 defmodule EarmarkParser.Parser.ListInfo do
 
-  alias EarmarkParser.{Options, Line}
+  alias EarmarkParser.{Options, Line, Line.ListItem}
 
   import EarmarkParser.Helpers.LookaheadHelpers, only: [opens_inline_code: 1, still_inline_code: 2]
 
@@ -8,33 +8,36 @@ defmodule EarmarkParser.Parser.ListInfo do
 
   @not_pending {nil, 0}
 
-  defstruct(
+  defstruct [
     indent: 0,
     lines: [],
     loose?: false,
     pending: @not_pending,
-    options: %EarmarkParser.Options{},
+    options: %Options{},
     width: 0
-  )
+  ]
+
   @type t :: %__MODULE__{
     indent: non_neg_integer(),
-    lines: list(),
+    lines: [String.t()],
     loose?: boolean(),
     pending: {nil | String.t(), non_neg_integer()},
     options: Options.t(),
     width: non_neg_integer()
   }
 
-  @spec new(Line.ListItem.t(), Options.t()) :: t()
-  def new(%Line.ListItem{initial_indent: ii, list_indent: width} = item, options) do
+  @spec new(ListItem.t(), Options.t()) :: t()
+  def new(%ListItem{initial_indent: ii, list_indent: width} = item, options) do
     pending = opens_inline_code(item)
     %__MODULE__{indent: ii, lines: [item.content], options: options, pending: pending, width: width}
   end
 
+  @spec update_list_info(t(), String.t(), Line.t(), boolean()) :: t()
   def update_list_info(list_info, line, pending_line, loose? \\ false) do
     prepend_line(list_info, line) |> _update_rest(pending_line, loose?)
   end
 
+  @spec prepend_line(t(), String.t()) :: t()
   def prepend_line(%__MODULE__{lines: lines} = list_info, line) do
     %{list_info | lines: [line | lines]}
   end
