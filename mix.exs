3449a26eb4b18cad3bd45deebb12858fb6fb85a4
RobertDober <robert.dober@gmail.com>
2025-10-25 12:38:34 +0200

    Fixing mix.exs deprecation warning

--- mix.exs
+++ mix.exs
@@ -18,6 +18,17 @@ defmodule EarmarkParser.MixProject do
     {:floki, "~> 0.36", only: [:dev, :test]}
   ]
 
+  def cli do
+    [
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
+    ]
+  end
+
   def project do
     [
       app: :earmark_parser,
@@ -28,12 +39,6 @@ defmodule EarmarkParser.MixProject do
       deps: @deps,
       description: "AST parser and generator for Markdown",
       package: package(),
-      preferred_cli_env: [
-        coveralls: :test,
-        "coveralls.detail": :test,
-        "coveralls.post": :test,
-        "coveralls.html": :test
-      ],
       test_coverage: [tool: ExCoveralls],
       aliases: [docs: &build_docs/1]
     ]
